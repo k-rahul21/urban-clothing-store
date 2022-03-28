f827aeb5b54d239a50e50d30c922e18971418d9b
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2022-03-28 14:26:18 +0530

    add: export functionality

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,16 +1,22 @@
-<footer class="footer-section">
-  <h1 class="heading3 font-bold">Experience Urban-Clothing app on mobile</h1>
-  <div class="download-app-links">
-    <img
-      src="/assets/google-play-badge.png"
-      alt="Playstore app link"
-      class="playstore-link"
-    />
-    <img
-      src="/assets/app-store-badge.png"
-      alt="Appstore app link"
-      class="appstore-link"
-    />
-  </div>
-  <h4 class="origin">INDIA</h4>
-</footer>;
+export const Footer = () => {
+  return (
+    <footer class="footer-section">
+      <h1 class="heading3 font-bold">
+        Experience Urban-Clothing app on mobile
+      </h1>
+      <div class="download-app-links">
+        <img
+          src="/assets/google-play-badge.png"
+          alt="Playstore app link"
+          class="playstore-link"
+        />
+        <img
+          src="/assets/app-store-badge.png"
+          alt="Appstore app link"
+          class="appstore-link"
+        />
+      </div>
+      <h4 class="origin">INDIA</h4>
+    </footer>
+  );
+};
