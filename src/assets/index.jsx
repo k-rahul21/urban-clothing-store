cfea270b2068893fa1d4a72942cff86e647a07be
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2022-05-06 14:21:17 +0530

    update: assets or db

---
+++ src/assets/index.jsx
@@ -0,0 +1,58 @@
+import iosBadge from "./Products/badge/app-store-badge.png";
+import androidBadge from "./Products/badge/google-play-badge.png";
+
+import shoe1 from "./Products/shoes/shoe1.jpg";
+import shoe2 from "./Products/shoes/shoe2.jpg";
+import shoe3 from "./Products/shoes/shoe3.jpg";
+import shoe4 from "./Products/shoes/shoe4.jpg";
+import shoe5 from "./Products/shoes/shoe5.jpg";
+import shoe6 from "./Products/shoes/shoe6.jpg";
+import shoe7 from "./Products/shoes/shoe7.jpg";
+import shoe8 from "./Products/shoes/shoe8.jpg";
+import shoeCover from "./Products/categories-cover/shoes-cover.avif";
+
+import jacket1 from "./Products/jackets/jacket1.jpg";
+import jacket2 from "./Products/jackets/jacket2.jpg";
+import jacket3 from "./Products/jackets/jacket3.jpg";
+import jacket4 from "./Products/jackets/jacket4.jpg";
+import jacket5 from "./Products/jackets/jacket5.jpg";
+import jacket6 from "./Products/jackets/jacket6.jpg";
+import jacket7 from "./Products/jackets/jacket7.jpg";
+import jacket8 from "./Products/jackets/jacket8.jpg";
+import jacketCover from "./Products/categories-cover/jackets-cover.avif";
+
+import accessory1 from "./Products/accessories/accessory1.jpg";
+import accessory2 from "./Products/accessories/accessory2.jpg";
+import accessory3 from "./Products/accessories/accessory3.jpg";
+import accessory4 from "./Products/accessories/accessory4.jpg";
+import accessory5 from "./Products/accessories/accessory5.jpg";
+import accessory6 from "./Products/accessories/accessory6.jpg";
+import accessory7 from "./Products/accessories/accessory7.jpg";
+import accessory8 from "./Products/accessories/accessory8.jpg";
+import accessoryCover from "./Products/categories-cover/accessoriess-cover.avif";
+
+export { shoe1, shoe2, shoe3, shoe4, shoe5, shoe6, shoe7, shoe8, shoeCover };
+export {
+  jacket1,
+  jacket2,
+  jacket3,
+  jacket4,
+  jacket5,
+  jacket6,
+  jacket7,
+  jacket8,
+  jacketCover,
+};
+export {
+  accessory1,
+  accessory2,
+  accessory3,
+  accessory4,
+  accessory5,
+  accessory6,
+  accessory7,
+  accessory8,
+  accessoryCover,
+};
+
+export { iosBadge, androidBadge };
